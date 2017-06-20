b1534c48ff63c3aa8d3a46a3db432d3c2164ec35
AronLowe <lowe.aron@gmail.com>
2017-06-19 20:17:58 -0500

    added links for tabs and passed tests

--- spec/views/admins/index.html.erb_spec.rb
+++ spec/views/admins/index.html.erb_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "admins/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "renders the correct links" do
+    # setup
+
+    # action
+    thing = render :template => "admins/index.html"
+
+    # assertions
+    expect(thing).to include("Admin")
+    expect(rendered).to include("Profile")
+    expect(rendered).to include("Beds")
+    expect(rendered).to include("Residents")
+    expect(rendered).to include("Meals")
+  end
 end
