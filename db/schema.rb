5e82afb9d4bf6ca8f98c237d7d8891d7e3d09619
Kyle Gundry <Gundr1kt@gmail.com>
2017-06-26 19:24:06 -0500

    create chores controllers, models, and views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170619233506) do
+ActiveRecord::Schema.define(version: 20170626235318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,12 @@ ActiveRecord::Schema.define(version: 20170619233506) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_admins_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
+  end
+
+  create_table "chores", force: :cascade do |t|
+    t.string   "task"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
