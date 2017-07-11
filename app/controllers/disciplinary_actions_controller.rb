0ed07522ca30e59da3a19b70f92b23319f06de79
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-07-10 19:44:56 -0500

    add disciplinary action

--- app/controllers/disciplinary_actions_controller.rb
+++ app/controllers/disciplinary_actions_controller.rb
@@ -3,6 +3,18 @@ class DisciplinaryActionsController < ApplicationController
     render "new.html.erb"
   end
   def create 
-    redirect_to "/"
+    disciplinary_action = DisciplinaryAction.new(
+        admin_id: current_admin.id,
+        resident_id: Resident.find_by(id: params[:id]),
+        incident_name: params[:incident_name],
+        incident_description: params[:incident_description],
+        date: Time.now
+      )
+    if disciplinary_action.save
+      render "create.html.erb"
+    else
+      flash[:warning] = "You need to fill out the whole form!"
+        render "new.html.erb"
+    end
   end
 end
